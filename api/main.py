8b597b50aafc08296237ee93c4f69822d1678984
Artemis <86613642+artemis283@users.noreply.github.com>
2025-02-26 12:03:55 +0000

    Update main.py
    fixing model issue

--- api/main.py
+++ api/main.py
@@ -3,8 +3,22 @@ import torch.nn.functional as F
 from fastapi import FastAPI
 from pydantic import BaseModel
 import numpy as np
+class DigitClassifier(nn.Module):
+    def __init__(self):
+        super(DigitClassifier, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, 3, 1)
+        self.conv2 = nn.Conv2d(32, 64, 3, 1)
+        self.fc1 = nn.Linear(64 * 24 * 24, 128)
+        self.fc2 = nn.Linear(128, 10)
 
-from model.train import DigitClassifier
+    def forward(self, x):
+        x = torch.relu(self.conv1(x))
+        x = torch.relu(self.conv2(x))
+        x = torch.flatten(x, 1)
+        x = torch.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+    
 model = DigitClassifier()
 model.load_state_dict(torch.load("mnist_cnn.pth"))
 model.eval()
